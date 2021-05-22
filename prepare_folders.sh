7f792841665a15a04a755590f5011235f1ce1940
Firas Baba <firas.baba96@gmail.com>
2021-05-23 01:00:51 +0200

    first commit

---
+++ prepare_folders.sh
@@ -0,0 +1,15 @@
+mkdir input
+mkdir input/folds
+mkdir weights
+mkdir notebooks
+mkdir working
+mkdir submissions
+
+# Python files
+touch working/config.py
+touch working/dataset.py
+touch working/models.py
+touch working/predict.py
+touch working/train.py
+touch working/validation.py
+touch working/prepare_data.py
