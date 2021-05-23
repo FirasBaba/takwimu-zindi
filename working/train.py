1c265e367435aeda7b6bf01fa219d2feb590eea6
Firas Baba <firas.baba96@gmail.com>
2021-05-23 05:26:48 +0200

    use loss to save best weights

--- working/train.py
+++ working/train.py
@@ -98,39 +98,36 @@ model.to(device)
 best_loss = 9999
 for counter in range(config.n_epochs):
 
-    # epoch_iterator_train = tqdm(train_dl)
-    # tr_loss = 0.0
-    # for step, batch in enumerate(epoch_iterator_train):
-    #     model.train()
-    #     source_ids, source_mask = batch["source_ids"].to(device), batch["source_mask"].to(device)
-    #     target_ids, target_mask = batch["target_ids"].to(device), batch["target_mask"].to(device)
+    epoch_iterator_train = tqdm(train_dl)
+    tr_loss = 0.0
+    for step, batch in enumerate(epoch_iterator_train):
+        model.train()
+        source_ids, source_mask = batch["source_ids"].to(device), batch["source_mask"].to(device)
+        target_ids, target_mask = batch["target_ids"].to(device), batch["target_mask"].to(device)
 
-    #     target_ids[target_ids[:, :] == tokenizer.pad_token_id] = -100
+        target_ids[target_ids[:, :] == tokenizer.pad_token_id] = -100
 
-    #     outputs = model(input_ids=source_ids,
-    #         attention_mask=source_mask,
-    #         decoder_input_ids=target_ids,
-    #         decoder_attention_mask=target_mask)
+        outputs = model(input_ids=source_ids,
+            attention_mask=source_mask,
+            decoder_input_ids=target_ids,
+            decoder_attention_mask=target_mask)
 
-    #     loss = outputs[0]
+        loss = outputs[0]
 
 
-    #     (loss).backward()
-    #     if (step + 1) % config.gradient_accumulation_steps == 0:
-    #         optimizer.step()
-    #         scheduler.step()  # Update learning rate schedule
-    #         model.zero_grad()
+        (loss).backward()
+        if (step + 1) % config.gradient_accumulation_steps == 0:
+            optimizer.step()
+            scheduler.step()  # Update learning rate schedule
+            model.zero_grad()
 
-    #     tr_loss += loss.item()
-    #     epoch_iterator_train.set_postfix(batch_loss=(loss.item()), loss=(tr_loss/(step+1)))
+        tr_loss += loss.item()
+        epoch_iterator_train.set_postfix(batch_loss=(loss.item()), loss=(tr_loss/(step+1)))
 
-    # print(f"EPOCH {counter}/{config.n_epochs}: Training average loss: {tr_loss/(step+1)}")
-    model.load_state_dict(torch.load("weights/first_.pth"))
+    print(f"EPOCH {counter}/{config.n_epochs}: Training average loss: {tr_loss/(step+1)}")
 
-    if config.validation and (counter%4 == 3):
+    if config.validation:
         print("Predicting on the validation set")
-        les_scores = []
-        scorer = rouge_scorer.RougeScorer(['rouge1'], use_stemmer=True)
         val_loss = 0.0
         epoch_iterator_val = tqdm(validation_dl)
         for  step, batch in enumerate(epoch_iterator_val):
@@ -140,36 +137,14 @@ for counter in range(config.n_epochs):
 
             target_ids[target_ids[:, :] == tokenizer.pad_token_id] = -100
 
-            original_sentence = batch["original_sentence"][0]
-
-            beam_outputs = model.translator.generate(
-                input_ids=source_ids, attention_mask=source_mask,
-                do_sample=True,
-                max_length=100,
-                top_k=120,
-                top_p=0.98,
-                early_stopping=True,
-                num_return_sequences=1,
-                num_beams= 3,
-                )
-            for beam_output in beam_outputs:
-                sent = tokenizer.decode(beam_output, skip_special_tokens=True,clean_up_tokenization_spaces=True)
-
-            # print(sent)
-            current_score = scorer.score(sent,
-                original_sentence)["rouge1"].fmeasure
-
     
-            les_scores.append(current_score)
-
             outputs = model(input_ids=source_ids,
                 attention_mask=source_mask,
                 decoder_input_ids=target_ids,
                 decoder_attention_mask=target_mask)
             loss = outputs[0]
             val_loss += loss.item()
-            epoch_iterator_val.set_postfix(batch_loss=(loss.item()), loss=(val_loss/(step+1)),\
-                mean=np.mean(les_scores), last_score=current_score)
+            epoch_iterator_val.set_postfix(batch_loss=(loss.item()), loss=(val_loss/(step+1)))
         print(f"EPOCH {counter}/{config.n_epochs}: Validation average loss: {val_loss/(step+1)}")
         if val_loss < best_loss:
             print("Saving the Translator...")
