a36bef81c53ffd1d2624e6cf980cdc7e24bbddb7
Firas Baba <firas.baba96@gmail.com>
2021-05-23 06:31:29 +0200

    download jw300 data

--- prepare_folders.sh
+++ prepare_folders.sh
@@ -13,3 +13,6 @@ touch working/predict.py
 touch working/train.py
 touch working/validation.py
 touch working/prepare_data.py
+
+opus_read -d JW300 -s fr -t fon -wm moses -w input/jw300_fon.fr input/jw300.fon
+opus_read -d JW300 -s fr -t ee -wm moses -w input/jw300_ee.fr input/jw300.ee
